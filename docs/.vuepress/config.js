cdcf8e6625a36f12285a31ff6c42c8816040d08e
chao <vchenghou@aliyun.com>
2022-11-08 20:26:51 +0800

    更新文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,10 +3,12 @@ module.exports = {
   description: '基于VUE的拖拽式数据报表设计器',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
     ['link', { rel: 'icon', href: '/logo.ico' }],
+    ['meta', { name: 'cola designer', content: 'cola designer cola-designer Cola-Designer'}]
   ],
   base: '/cola-designer-doc/',
   markdown: {
-    lineNumbers: false // 代码块显示行号
+    lineNumbers: false, // 代码块显示行号
+    externalLinks: { target: '_blank', rel: 'nofollow noopener noreferrer' }
   },
   themeConfig: {
     nav:[
